4538b89dfe91d0d864762b9c846ca90e35b53ce7
wakary <redoume@163.com>
2018-05-23 22:00:20 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-MYPMarkdownEditor_Example/Pods-MYPMarkdownEditor_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MYPMarkdownEditor_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MYPMarkdownEditor_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-MYPMarkdownEditor_Tests/Pods-MYPMarkdownEditor_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MYPMarkdownEditor_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MYPMarkdownEditor_Tests
+@end
