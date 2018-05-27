bbafe12353ee4d3581e88d0d15b7ebaf8c300801
wakary <redoume@163.com>
2018-05-27 22:08:33 +0800

    update

--- MYPMarkdownEditor/Classes/MYPStyleSettingController.swift
+++ MYPMarkdownEditor/Classes/MYPStyleSettingController.swift
@@ -7,9 +7,14 @@
 
 import UIKit
 
-class MYPStyleSettingController: UITableViewController {
+open class MYPStyleSettingController: UITableViewController {
+    
+    weak var delegate: MYPStyleSettingProtocol?
+    
+    private var selectedIndexPath: IndexPath?
+    private var shouldScrollToSelectedRow = false
 
-    override func viewDidLoad() {
+    override open func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -18,77 +23,111 @@ class MYPStyleSettingController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    open func reload() {
+        self.tableView.reloadData()
+    }
 
-    override func didReceiveMemoryWarning() {
+    override open func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension MYPStyleSettingController: MYPStyleSettingProtocol {
+    public func mypMarkdownEditor_didTap(styleType: MYPMarkdownEditorFormattingStyle, info: [String : Any]?) {
+        
+    }
+}
+
+extension MYPStyleSettingController {
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    
+    override open func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    open override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath == self.selectedIndexPath {
+            switch indexPath.row {
+            case 0:
+                return 120.0
+            case 3:
+                return 120.0
+            case 4:
+                return 180.0
+            default:
+                break
+            }
+        }
+        return 60.0
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "headerID", for: indexPath) as! MYPStyleHeaderCell
+            cell.delegate = self
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "fontStyleID", for: indexPath) as! MYPStyleFontStyleCell
+            cell.delegate = self
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "paragraphID", for: indexPath) as! MYPStyleParagraphCell
+            cell.delegate = self
+            return cell
+        case 3:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "fontSizeID", for: indexPath) as! MYPStyleFontSizeCell
+            cell.delegate = self
+            return cell
+        case 4:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "colorID", for: indexPath) as! MYPStyleColorCell
+            cell.delegate = self
+            return cell
+        default:
+            break
+        }
+     
+     // Configure the cell...
+     let cell = UITableViewCell(style: .default, reuseIdentifier: "XX")
+     return cell
+     }
+    
+    open override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath == self.selectedIndexPath {
+            cell.isSelected = true
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    open override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if self.shouldScrollToSelectedRow && indexPath == self.selectedIndexPath {
+            tableView.scrollToRow(at: indexPath, at: .none, animated: true)
+            self.shouldScrollToSelectedRow = false
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    open override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var indexPaths = [IndexPath]()
+        if indexPath == self.selectedIndexPath {
+            self.selectedIndexPath = nil
+        }
+        else {
+            if self.selectedIndexPath != nil {
+                indexPaths.append(self.selectedIndexPath!)
+            }
+            self.selectedIndexPath = indexPath
+        }
+        indexPaths.append(indexPath)
+        self.shouldScrollToSelectedRow = true
+        tableView.reloadRows(at: indexPaths, with: .automatic)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
